e328e0562f2b114a9c95e195227b6845772ee7e5
rj <rj@codecov.io>
2023-12-05 11:53:14 -0600

    setting up Sentry

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import _ from 'lodash';
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+  dsn: "https://7d881b4dd4254a0a94f449045cfd5882@o26192.ingest.sentry.io/74037",
+
+  // Alternatively, use `process.env.npm_package_version` for a dynamic release version
+  // if your build tool supports it.
+  release: "rj-onboarding@0.0.1",
+  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+
+  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 function component() {
   const element = document.createElement('div');
@@ -8,5 +31,9 @@ function component() {
 
   return element;
 }
+
+setTimeout(() => {
+  throw new Error("Sentry Test Error");
+});
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
+const {
+  sentryWebpackPlugin
+} = require("@sentry/webpack-plugin");
+
 const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
+  devtool: "source-map",
+
+  plugins: [sentryWebpackPlugin({
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+    org: "rj-demo-org",
+    project: "rj-onboarding"
+  })]
 };
